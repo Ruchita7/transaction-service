c4f84d13f32e270e35d438f3bb44822de3c78936
Apple <apple@Apples-MBP.lan>
2026-08-27 00:32:38 +0530

    Added swagger implementation

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Global Configs
 
 https://gist.github.com/Ruchita7/df3276df494795c49b327b255c0ab46e
+
+[Swagger Link ](http://localhost:8081/api/v1/swagger-ui/index.html)
--- src/main/java/com/example/banking/controller/TransactionRestController.java
+++ src/main/java/com/example/banking/controller/TransactionRestController.java
@@ -5,12 +5,16 @@ import com.example.banking.dto.TransactionDTO;
 import com.example.banking.service.TransactionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/transactions")
+@Tag(name = "Transaction Management", description = "APIs for managing application transactions")
 public class TransactionRestController {
 
     private TransactionService transactionService;
@@ -20,11 +24,23 @@ public class TransactionRestController {
     }
 
     @GetMapping("/{transactionId}")
+    @Operation(
+            summary = "Get transaction by ID",
+            description = "Provides full transaction details based on the unique transaction database ID."
+    )
+    @ApiResponse(responseCode = "200", description = "Transaction successfully found")
+    @ApiResponse(responseCode = "404", description = "Transaction not found")
     public ResponseEntity<TransactionDTO> getTransactionById(@PathVariable(name = "transactionId") UUID transactionId) {
         return ResponseEntity.ok(transactionService.findById(transactionId));
     }
 
     @GetMapping("/account/{accountNumber}")
+    @Operation(
+            summary = "Get transaction by account",
+            description = "Provides full transaction details based on the unique account number."
+    )
+    @ApiResponse(responseCode = "200", description = "Transaction successfully found")
+    @ApiResponse(responseCode = "404", description = "Transaction not found")
     public ResponseEntity<List<TransactionDTO>> getTransactionsByAccountNumber(@PathVariable(name = "accountNumber")
                                                                                String accountNumber) {
         return ResponseEntity.ok(transactionService.getTransactionHistory(accountNumber));
@@ -32,16 +48,34 @@ public class TransactionRestController {
 
 
     @PostMapping("/transfer")
+    @Operation(
+            summary = "Transfer",
+            description = "Perform transfer of funds."
+    )
+    @ApiResponse(responseCode = "200", description = "Transaction successfully performed")
+    @ApiResponse(responseCode = "404", description = "Transaction not performed")
     public ResponseEntity<TransactionDTO> transferFunds(@RequestBody  TransactionDTO transactionDTO) {
         return ResponseEntity.ok(transactionService.processTransaction(transactionDTO));
     }
 
     @PostMapping("/withdraw")
+    @Operation(
+            summary = "Withdrew",
+            description = "Perform withdrawal of funds."
+    )
+    @ApiResponse(responseCode = "200", description = "Withdrawal successfully performed")
+    @ApiResponse(responseCode = "404", description = "Withdrawal not performed")
     public ResponseEntity<TransactionDTO> withdraw(@RequestBody TransactionDTO transactionDTO) {
         return ResponseEntity.ok(transactionService.processTransaction(transactionDTO));
     }
 
     @PostMapping("/deposit")
+    @Operation(
+            summary = "Deposit",
+            description = "Perform deposit of funds."
+    )
+    @ApiResponse(responseCode = "200", description = "Deposit successfully performed")
+    @ApiResponse(responseCode = "404", description = "Deposit not performed")
     public ResponseEntity<TransactionDTO> deposit(@RequestBody TransactionDTO transactionDTO) {
         return ResponseEntity.ok(transactionService.processTransaction(transactionDTO));
     }
